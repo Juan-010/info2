632f63e6aac5f4f4a2a7a4b7e496a3e10089162e
Juan-010 <juanchijujuy5@gmail.com>
2021-08-02 20:00:16 -0300

    comentarios

--- testeo/heap-stack.c
+++ testeo/heap-stack.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void){
+  //Stack
   int a = 10, b = 15, c = 20;
   int *ptra = &a;
   int *ptrb = &b;
   int *ptrc = &c;
+  
+  //Heap
   int *ptrd = (int *)malloc(sizeof(int));
   int *ptre = (int *)malloc(sizeof(int));
   int *ptrf = (int *)malloc(sizeof(int));
   *ptrd = 25;
   *ptre = 30;
   *ptrf = 35;
+  
   printf("ptra (Stack): %p\n", ptra);
   printf("ptrb (Stack): %p\n", ptrb);
   printf("ptrc (Stack): %p\n", ptrc);
