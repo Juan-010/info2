1806afb915993b6295e54f54af3292b17f6cc7c3
Juan Ignacio Sanchez Serantes <juan@Air-de-Juan.fibertel.com.ar>
2021-10-18 21:10:44 -0300

    Primeros ejemplos cpp

---
+++ cpp/hora.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Hora{
+	public:
+		Hora();
+		void setHora(int, int, int);
+		void printMilitar();
+		void printEstandar();
+	
+	private:
+		int hora; // 0 - 23
+		int minuto; // 0 - 59
+		int segundo; // 0 - 59
+};
+
+Hora::Hora(){
+	hora = minuto = segundo = 0;
+}
+
+void Hora::setHora(int h, int m, int s){
+	hora = (h >= 0 && h < 24) ? h : 0;
+	minuto = (m >= 0 && m < 60) ? m : 0;
+	segundo = (s >= 0 && s < 60) ? s : 0;
+}
+
+void Hora::printMilitar(){
+	cout << (hora < 10 ? "0" : "") << hora << ":" 
+		<< (minuto < 10 ? "0" : "") << minuto << ":" 
+		<< (segundo < 10 ? "0" : "") << segundo;
+}
+
+void Hora::printEstandar(){
+	cout << ((hora == 0 | hora == 12) ? 12 : hora % 12) << ":" 
+		<< (minuto < 10 ? "0" : "") << minuto << ":" 
+		<< (segundo < 10 ? "0" : "") << segundo 
+		<< (hora < 12 ? " AM" : " PM");	
+}
+
+int main(){
+	Hora h;
+	h.setHora(17,25,17);
+	
+	cout << "La hora (Militar) es: ";
+	h.printMilitar();
+	cout << endl;
+
+	cout << "La hora (Estandar) es : ";
+	h.printEstandar();
+	cout << endl;
+	return 0;
+}
